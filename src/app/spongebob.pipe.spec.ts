24fc7f5f6a1631875ab12abdbcc44c728cd9fb0e
Graham <greysnaa@gmail.com>
2019-09-09 14:34:20 -0500

    functionality testing

--- src/app/spongebob.pipe.spec.ts
+++ src/app/spongebob.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { SpongebobPipe } from './spongebob.pipe';
+import { TestBed } from '@angular/core/testing';
 
 describe('SpongebobPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SpongebobPipe();
+  const pipe = new SpongebobPipe();
+  it('exists', () => {
     expect(pipe).toBeTruthy();
   });
+
+  it('transforms "Spongebob" to "sPoNgEbOb"', () => {
+    expect(pipe.transform('Spongebob')).toBe('sPoNgEbOb');
+  });
+  
+  it('transforms "Spongebob Square" to "sPoNgEbOb SqUaRe"', () => {
+    expect(pipe.transform('Spongebob Square')).toBe('sPoNgEbOb SqUaRe');
+  });
+
+  // it('should convert todoBody to SpongebobCase', () => {
+  //   const fixture = TestBed.createComponent(TodoComponent);
+  //   const hostElement = fixture.nativeElement;
+  // });
 });
+
+// Check to see if casing works in transform function
+// Check to see if it works in a component
