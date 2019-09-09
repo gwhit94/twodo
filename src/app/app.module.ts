162585071c51a67caf7f67936dd353f05003fbef
Graham <greysnaa@gmail.com>
2019-09-09 13:38:02 -0500

    generated starting files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { SpongebobPipe } from './spongebob.pipe';
+import { MarkDirective } from './mark.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    SpongebobPipe,
+    MarkDirective
   ],
   imports: [
     BrowserModule,
