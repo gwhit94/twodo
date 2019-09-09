5f77f481ada3e04ae50ea970ac6c2abcae7836aa
Graham <greysnaa@gmail.com>
2019-09-09 14:38:28 -0500

    pipe functionality, spec passing

--- src/app/spongebob.pipe.ts
+++ src/app/spongebob.pipe.ts
@@ -8,15 +8,15 @@ export class SpongebobPipe implements PipeTransform {
   transform(value: string): string {
     let isCap = false;
     return value.split("").map(val => {
-        if (val === " ") {
-            return val;
-        }
-        val = isCap ? val.toUpperCase() : val.toLowerCase();
-        isCap = !isCap;
+      if (val === " ") {
         return val;
+      }
+      val = isCap ? val.toUpperCase() : val.toLowerCase();
+      isCap = !isCap;
+      return val;
     }).join("");
 
-}
+  }
 
 }
 // Pipe that adds spongebob case
